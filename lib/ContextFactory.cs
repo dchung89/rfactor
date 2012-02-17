ea991a8aff40c95043643ccae03e1fe58aa19055
Casper Holmgreen <cholmgreen@gmail.com>
2012-02-17 14:10:33 -0500

    Finished rudimentary ContextFactory (model)

---
+++ lib/ContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roslyn.Services;
+using Roslyn.Services.Editor;
+
+namespace rfactor.lib
+{
+    class ContextFactory
+    {
+        IWorkspace iWorkspace;
+        ISolution iSolution;
+
+        public ContextFactory(IWorkspace iwork, ISolution isol)
+        {
+            this.iWorkspace = iwork;
+            this.iSolution = isol;
+        }
+
+        public IWorkspace GetIWorkspace()
+        {
+            return this.iWorkspace;
+        }
+
+        public ISolution GetISolution()
+        {
+            return this.iSolution;
+        }
+}
