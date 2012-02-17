61fbb3aa9e29b1fb06ad5ca316d29e3fa78f2655
Casper Holmgreen <cholmgreen@gmail.com>
2012-02-17 13:21:11 -0500

    Unfinished rename_local_variable.cs

---
+++ lib/refactorings/rename_local_variable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+using Roslyn.Services;
+using Roslyn.Services.Editor;
+
+namespace rfactor.lib.refactorings
+{
+    class rename_local_variable
+    {
+        [Import]
+        private IRenameService iRenameService;
+        public bool CheckPreconditions()
+        {
+            throw new NotImplementedException();
+        }
+        public SyntaxTree Refactor()
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
